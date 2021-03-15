79d42e566b9240192c42d398ac2a63b20de740bd
Adil <adilqerim@gmail.com>
2021-03-15 13:14:35 +0300

    Lesson 27 | Practice props and use debugger

--- src/App.js
+++ src/App.js
@@ -28,3 +28,4 @@ const App = (props) => {
 }
 
 export default App;
+
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,7 +4,6 @@ import Post from './Post/Post';
 
 
 const MyPosts = (props) => {
-
     const postsElements = props.posts
         .map(p => <Post message={p.text} likesCount={p.likesCount}/>)
 
