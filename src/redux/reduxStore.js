4ca8ddcfd6889dc4dd703d2de2daec187b693229
Adil <adilqerim@gmail.com>
2021-03-23 11:55:24 +0300

    Lesson 49 | Add usersPage

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -2,10 +2,12 @@ import {combineReducers, createStore} from "redux";
 import profileReducer from "./profileReducer";
 import dialogsReducer from "./dialogsReduce";
 import sidebarReduce from "./sidebarReduce";
+import usersReduce from "./usersReduce";
 
 const reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
+    usersPage: usersReduce,
     sidebar: sidebarReduce
 });
 
