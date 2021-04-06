a4e95ae501bd68d566f92fd0ec75de7549afbd90
Adil <adilqerim@gmail.com>
2021-04-06 16:43:28 +0300

    Lesson 60 | withRouter, props.match.params

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import userPhoto from '../../../assets/images/user.png'
 
 const ProfileInfo = (props) => {
 
@@ -10,15 +11,13 @@ const ProfileInfo = (props) => {
     return (
         <div>
             <div>
-                <img
-                    src='https://images.unsplash.com/photo-1578608738964-cd27acd5af2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1291&q=80'/>
+                <img src='https://images.unsplash.com/photo-1578608738964-cd27acd5af2c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1291&q=80'/>
             </div>
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large}/>
+                <img className={s.userAvatar} src={props.profile.photos.large !== null ? props.profile.photos.large : userPhoto}/>
                 <p> {props.profile.fullName}</p>
                 <p> {props.profile.aboutMe}</p>
                 <p> {props.profile.contacts.vk}</p>
-                ava + description
             </div>
         </div>
     )
