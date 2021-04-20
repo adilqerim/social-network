d7bb87b857ec8826f27180a4d1a247f52231e918
Adil <adilqerim@gmail.com>
2021-04-20 19:54:57 +0300

    Updates

--- precache-manifest.5928fb228ea3c18df8b033c5733e6127.js
+++ precache-manifest.5928fb228ea3c18df8b033c5733e6127.js
@@ -4,12 +4,12 @@ self.__precacheManifest = [
     "url": "/social-network/static/js/0.02df00d9.chunk.js"
   },
   {
-    "revision": "2d197ca6c47fe0f6b85f",
+    "revision": "910f517cda31eb46f3b2",
     "url": "/social-network/static/css/main.dee81c41.chunk.css"
   },
   {
-    "revision": "2d197ca6c47fe0f6b85f",
-    "url": "/social-network/static/js/main.2d197ca6.chunk.js"
+    "revision": "910f517cda31eb46f3b2",
+    "url": "/social-network/static/js/main.910f517c.chunk.js"
   },
   {
     "revision": "dcd8a756ec90b97854d3",
@@ -20,20 +20,20 @@ self.__precacheManifest = [
     "url": "/social-network/static/js/2.dcd8a756.chunk.js"
   },
   {
-    "revision": "94fa41c92b1a8279bf46",
+    "revision": "2cbb0082d8671c579517",
     "url": "/social-network/static/css/3.d7752bcd.chunk.css"
   },
   {
-    "revision": "94fa41c92b1a8279bf46",
-    "url": "/social-network/static/js/3.94fa41c9.chunk.js"
+    "revision": "2cbb0082d8671c579517",
+    "url": "/social-network/static/js/3.2cbb0082.chunk.js"
   },
   {
-    "revision": "634a419b8aac1ec6846f",
+    "revision": "c5c544932cbbadfaaa0c",
     "url": "/social-network/static/css/4.43b6783e.chunk.css"
   },
   {
-    "revision": "634a419b8aac1ec6846f",
-    "url": "/social-network/static/js/4.634a419b.chunk.js"
+    "revision": "c5c544932cbbadfaaa0c",
+    "url": "/social-network/static/js/4.c5c54493.chunk.js"
   },
   {
     "revision": "e87c0e043aa1e40384a5",
@@ -44,19 +44,19 @@ self.__precacheManifest = [
     "url": "/social-network/static/js/5.e87c0e04.chunk.js"
   },
   {
-    "revision": "8ae1c88c8fa7e0527582",
-    "url": "/social-network/static/js/6.8ae1c88c.chunk.js"
+    "revision": "519471d79589cd71b8b4",
+    "url": "/social-network/static/js/6.519471d7.chunk.js"
   },
   {
-    "revision": "85943a9a5a6d97843eda",
-    "url": "/social-network/static/js/runtime~main.85943a9a.js"
+    "revision": "195c56600e9d667ca4ed",
+    "url": "/social-network/static/js/runtime~main.195c5660.js"
   },
   {
     "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
     "url": "/social-network/static/media/user.c7b5f6a0.png"
   },
   {
-    "revision": "994ee313443b0dd60bb2e8a100b542d8",
+    "revision": "e928441ed4a356666a03f85c65bfe73b",
     "url": "/social-network/index.html"
   }
 ];
