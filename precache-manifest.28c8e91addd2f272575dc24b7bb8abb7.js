0dd0e392835940459791f35eb32fb5a2016754f1
Adil <adilqerim@gmail.com>
2021-04-20 19:14:30 +0300

    Updates

---
+++ precache-manifest.28c8e91addd2f272575dc24b7bb8abb7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "02df00d9ce31656c2bef",
+    "url": "/social-network/static/js/0.02df00d9.chunk.js"
+  },
+  {
+    "revision": "08508204ee345fd1a97d",
+    "url": "/social-network/static/css/main.dee81c41.chunk.css"
+  },
+  {
+    "revision": "08508204ee345fd1a97d",
+    "url": "/social-network/static/js/main.08508204.chunk.js"
+  },
+  {
+    "revision": "dcd8a756ec90b97854d3",
+    "url": "/social-network/static/css/2.761d8173.chunk.css"
+  },
+  {
+    "revision": "dcd8a756ec90b97854d3",
+    "url": "/social-network/static/js/2.dcd8a756.chunk.js"
+  },
+  {
+    "revision": "94fa41c92b1a8279bf46",
+    "url": "/social-network/static/css/3.d7752bcd.chunk.css"
+  },
+  {
+    "revision": "94fa41c92b1a8279bf46",
+    "url": "/social-network/static/js/3.94fa41c9.chunk.js"
+  },
+  {
+    "revision": "634a419b8aac1ec6846f",
+    "url": "/social-network/static/css/4.43b6783e.chunk.css"
+  },
+  {
+    "revision": "634a419b8aac1ec6846f",
+    "url": "/social-network/static/js/4.634a419b.chunk.js"
+  },
+  {
+    "revision": "e87c0e043aa1e40384a5",
+    "url": "/social-network/static/css/5.1fc7585b.chunk.css"
+  },
+  {
+    "revision": "e87c0e043aa1e40384a5",
+    "url": "/social-network/static/js/5.e87c0e04.chunk.js"
+  },
+  {
+    "revision": "8ae1c88c8fa7e0527582",
+    "url": "/social-network/static/js/6.8ae1c88c.chunk.js"
+  },
+  {
+    "revision": "85943a9a5a6d97843eda",
+    "url": "/social-network/static/js/runtime~main.85943a9a.js"
+  },
+  {
+    "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
+    "url": "/social-network/static/media/user.c7b5f6a0.png"
+  },
+  {
+    "revision": "7fe00be56e9e4a4298d22e034dd448d7",
+    "url": "/social-network/index.html"
+  }
+];
