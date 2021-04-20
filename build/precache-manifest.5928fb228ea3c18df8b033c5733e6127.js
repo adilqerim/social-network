12744580826c19d846d60d8e10a05ccd74f03c0c
Adil <adilqerim@gmail.com>
2021-04-20 20:09:21 +0300

    Lesson 95 | github pages deploy

---
+++ build/precache-manifest.5928fb228ea3c18df8b033c5733e6127.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "02df00d9ce31656c2bef",
+    "url": "/social-network/static/js/0.02df00d9.chunk.js"
+  },
+  {
+    "revision": "910f517cda31eb46f3b2",
+    "url": "/social-network/static/css/main.dee81c41.chunk.css"
+  },
+  {
+    "revision": "910f517cda31eb46f3b2",
+    "url": "/social-network/static/js/main.910f517c.chunk.js"
+  },
+  {
+    "revision": "dcd8a756ec90b97854d3",
+    "url": "/social-network/static/css/2.761d8173.chunk.css"
+  },
+  {
+    "revision": "dcd8a756ec90b97854d3",
+    "url": "/social-network/static/js/2.dcd8a756.chunk.js"
+  },
+  {
+    "revision": "2cbb0082d8671c579517",
+    "url": "/social-network/static/css/3.d7752bcd.chunk.css"
+  },
+  {
+    "revision": "2cbb0082d8671c579517",
+    "url": "/social-network/static/js/3.2cbb0082.chunk.js"
+  },
+  {
+    "revision": "c5c544932cbbadfaaa0c",
+    "url": "/social-network/static/css/4.43b6783e.chunk.css"
+  },
+  {
+    "revision": "c5c544932cbbadfaaa0c",
+    "url": "/social-network/static/js/4.c5c54493.chunk.js"
+  },
+  {
+    "revision": "e87c0e043aa1e40384a5",
+    "url": "/social-network/static/css/5.1fc7585b.chunk.css"
+  },
+  {
+    "revision": "e87c0e043aa1e40384a5",
+    "url": "/social-network/static/js/5.e87c0e04.chunk.js"
+  },
+  {
+    "revision": "519471d79589cd71b8b4",
+    "url": "/social-network/static/js/6.519471d7.chunk.js"
+  },
+  {
+    "revision": "195c56600e9d667ca4ed",
+    "url": "/social-network/static/js/runtime~main.195c5660.js"
+  },
+  {
+    "revision": "c7b5f6a007a193933d22f1b03bf2b43e",
+    "url": "/social-network/static/media/user.c7b5f6a0.png"
+  },
+  {
+    "revision": "e928441ed4a356666a03f85c65bfe73b",
+    "url": "/social-network/index.html"
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './index.css';
 import ReactDOM from "react-dom";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from "./App";
 import store from "./redux/reduxStore";
 import {Provider} from "react-redux";
 
 
-
 ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={ store }>
-            <App />
-        </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+        <HashRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </HashRouter>
+    , document.getElementById('root'));
 
 
 
